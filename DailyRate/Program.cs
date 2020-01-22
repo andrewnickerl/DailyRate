46fce923677d633aa0076d73552d48910c6bc52c
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-21 19:24:22 -0500

    Wrote readDouble, readInt, calculateFee, and writeFee functions in order to validate the calling of each of these functions in run()

--- DailyRate/Program.cs
+++ DailyRate/Program.cs
@@ -11,11 +11,30 @@ namespace DailyRate
         static void Main(string[] args)
         {
             (new Program()).run();
+            Console.ReadKey();
         }
 
         void run()
         {
-
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter the number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
         }
+
+        private double readDouble(string v)
+        {
+            Console.Write(v);
+            return Double.Parse(Console.ReadLine());
+        }
+
+        private int readInt(string v)
+        {
+            Console.Write(v);
+            return int.Parse(Console.ReadLine());
+        }
+
+        private double calculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+        private void writeFee(double v) => Console.WriteLine($"The consultant's fee is {v * 1.1}.");
     }
 }
